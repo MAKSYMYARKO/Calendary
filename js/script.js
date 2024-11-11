1aee5cc938524aadfad082034a8ee5acd0112e6f
Max <maksyaremenko40@gmail.com>
2024-11-11 21:22:50 +0100

    ChangePngToWebp

--- js/script.js
+++ js/script.js
@@ -46,11 +46,11 @@ function changeBackground(isDay) {
 function changeWeatherImage(weatherCondition) {
     const weatherIcon = document.querySelector(".weather-icon");
     if (weatherCondition === "Clear") {
-        weatherIcon.src = "/assets/img/clear.png"; 
+        weatherIcon.src = "/assets/img/clear.webp"; 
     } else if (weatherCondition === "Clouds") {
-        weatherIcon.src = "/assets/img/clouds.png"; 
+        weatherIcon.src = "/assets/img/clouds.webp"; 
     } else if (weatherCondition === "Rain") {
-        weatherIcon.src = "/assets/img/rain.png";  
+        weatherIcon.src = "/assets/img/rain.webp";  
     } else {
         console.error(erorr);
     }
